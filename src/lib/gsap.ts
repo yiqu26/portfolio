8d86d32b9f8f338013e24d9112ca1b8649d50f71
lanli <lanlin1999123@gmail.com>
2026-06-03 20:02:50 +0800

    feat: central gsap plugin registration

---
+++ src/lib/gsap.ts
@@ -0,0 +1,8 @@
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { SplitText } from 'gsap/SplitText'
+import { useGSAP } from '@gsap/react'
+
+gsap.registerPlugin(ScrollTrigger, SplitText, useGSAP)
+
+export { gsap, ScrollTrigger, SplitText, useGSAP }
