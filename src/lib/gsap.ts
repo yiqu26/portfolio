d9501b9e258ceb393c3f4635fa75e2483ffa6b9e
lanli <lanlin1999123@gmail.com>
2026-06-04 00:23:49 +0800

    feat: jetbrains mono + terminal css tokens + register TextPlugin

--- src/lib/gsap.ts
+++ src/lib/gsap.ts
@@ -1,8 +1,9 @@
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { SplitText } from 'gsap/SplitText'
+import { TextPlugin } from 'gsap/TextPlugin'
 import { useGSAP } from '@gsap/react'
 
-gsap.registerPlugin(ScrollTrigger, SplitText, useGSAP)
+gsap.registerPlugin(ScrollTrigger, SplitText, TextPlugin, useGSAP)
 
-export { gsap, ScrollTrigger, SplitText, useGSAP }
+export { gsap, ScrollTrigger, SplitText, TextPlugin, useGSAP }
