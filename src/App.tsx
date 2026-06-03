b25b854b051cad64d4368db0342d38d66618315f
lanli <lanlin1999123@gmail.com>
2026-06-03 20:03:30 +0800

    feat: app shell with grain + section order, section stubs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import './index.css'
+import Grain from './components/Grain'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import Marquee from './components/Marquee'
+import About from './components/About'
 import Projects from './components/Projects'
+import Skills from './components/Skills'
+import Marquee from './components/Marquee'
+import Contact from './components/Contact'
 
 export default function App() {
   return (
-    <div style={{ background: '#000', minHeight: '100vh' }}>
+    <div className="min-h-screen bg-black text-white">
+      <Grain />
       <Navbar />
       <Hero />
-      <Marquee />
+      <About />
       <Projects />
+      <Skills />
+      <Marquee />
+      <Contact />
     </div>
   )
 }
